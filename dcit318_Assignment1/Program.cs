e1a6e61348b1e2a1f8884596e30bdd624b1062dd
eli-bigman <nutsugah81@gmail.com>
2024-06-02 16:13:26 +0000

    add grade calculator class

--- dcit318_Assignment1/Program.cs
+++ dcit318_Assignment1/Program.cs
@@ -19,28 +19,76 @@
                 switch (choice)
                 {
                     case "1":
-                        //PrintSeparator();
-                        //GradeCalculator();
+                        PrintSeparator();
+                        GradeCalculator();
                         break;
                     case "2":
-                        //PrintSeparator();
+                        PrintSeparator();
                         //TicketPriceCalculator();
                         break;
                     case "3":
-                        //PrintSeparator();
+                        PrintSeparator();
                         //TriangleTypeIdentifier();
                         break;
                     case "4":
-                        Console.WriteLine("Exiting the program... 👋🏼👋🏼👋🏼");
+                        Console.WriteLine("\nExiting the program... 👋🏼👋🏼👋🏼\n");
                         return;
                     default:
-                        Console.WriteLine("❌ ❌ Invalid choice. Please enter 1, 2, 3, or 4.");
+                        Console.WriteLine("\nInvalid choice. Please enter 1, 2, 3, or 4.\n");
                         break;
                 }
-                //PrintSeparator();
+                PrintSeparator();
             }
         }
 
 
+        static void GradeCalculator()
+        {
+            while (true)
+            {
+                Console.Write("Enter a numerical grade between 0 and 100 (or type 'exit' to return to the main menu): ");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                {
+                    return;
+                }
+
+                if (int.TryParse(input, out int grade))
+                {
+                    
+                    if (grade >= 0 && grade <= 100)
+                    {
+                        
+                        string letterGrade = grade switch
+                        {
+                            >= 90 => "A",
+                            >= 80 => "B",
+                            >= 70 => "C",
+                            >= 60 => "D",
+                            _ => "F"
+                        };
+
+           
+                        Console.WriteLine($"The letter grade is: >> {letterGrade} \n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n Error: The grade must be between 0 and 100.\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\n Error: Invalid input. Please enter a numerical grade.\n");
+                }
+            }
+        }
+
+        
+        
+        static void PrintSeparator()
+        {
+            Console.WriteLine(new string('-', 40));
+        }
     }
 }
